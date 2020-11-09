0b82c3d9bd5849903ba0698c4e7a9aa09673ce12
nacicatus <drsaurabhsikka@gmail.com>
2020-11-09 16:54:54 +0100

    first commit

--- PythonCookbook.py
+++ PythonCookbook.py
@@ -1,15 +1,18 @@
-from math import cos, radians
+from math import cos, sin, radians
 
 # Create a string with spaces proportional to a cosine of x in degrees
 def make_dot_string(x):
     rad = radians(x)                             # cos works with radians
-    numspaces = int(20 * cos(radians(x)) + 20)   # scale to 0-40 spaces
-    st = ' ' * numspaces + 'o'                   # place 'o' after the spaces
+    numspaces = int(20 * sin(radians(x)) + 20)   # scale to 0-40 spaces
+    crxspaces = int(20 * cos(radians(x)) + 20)
+    st = ' ' * numspaces + 'o' + ' ' * crxspaces + 'x'   # place 'o' and 'x' after the spaces
     return st
 
+
 def main():
-    for i in range(0, 1800, 2):
+    for i in range(0, 1800, 12):
         s = make_dot_string(i)
         print(s)
+      
 
 main()
